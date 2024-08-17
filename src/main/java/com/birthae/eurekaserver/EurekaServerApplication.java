c714f3f14f655b414770f4a633049cee570f3bc3
Jeon Hyejin <hyejda00@naver.com>
2024-08-17 23:01:16 +0900

    init commit

---
+++ src/main/java/com/birthae/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.birthae.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
